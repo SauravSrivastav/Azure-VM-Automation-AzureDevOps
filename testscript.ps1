39d3b1616822dd8c1ed3ddd37489830af24e76f8
SauravSrivastav <sauravsrivastav2205@gmail.com>
2019-11-13 13:41:34 +0530

    first commit

---
+++ testscript.ps1
@@ -0,0 +1,6 @@
+﻿New-Item -Path “HKLM:\Software\AppX”Mkdir C:\AppX
+Invoke-WebRequest -Uri “https://teststoragedemo.blob.core.windows.net/scripts/winx64_12201_client.zip” -OutFile “C:\AppX\winx64_12201_client.zip”
+Expand-Archive C:\AppX\winx64_12201_client.zip -DestinationPath C:\AppX\winx64_12201_client
+& C:\AppX\winx64_12201_client\winx64_12201_client\client\setup.exe -silent -noconfig -responseFile C:\AppX\winx64_12201_client\winx64_12201_client\client\client.rsp
+
+
